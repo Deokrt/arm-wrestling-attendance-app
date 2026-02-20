edf846b73e5221a6d15adce857966058ae9d481a
Deokrt <teo12nti@gmail.com>
2026-02-20 16:57:43 +0200

    initial Android project setup

---
+++ app/src/main/java/com/example/skgarm/ArmSkgApp.kt
@@ -0,0 +1,7 @@
+package com.example.skgarm
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ArmSkgApp : Application()
---
+++ app/src/main/java/com/example/skgarm/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.skgarm
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.skgarm.ui.theme.SkgArmTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SkgArmTheme {   // everything in here is my UI
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SkgArmTheme {
+        Greeting("Android")
+    }
+}
